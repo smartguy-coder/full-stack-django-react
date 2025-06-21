6f4163779524e722a80fc8a69b00816e9012939c
Vasyl <vasyl@example.com>
2025-06-21 14:56:23 +0300

    video 5

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name}  - #{self.pk}"
+
+
+class League(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name}  - #{self.pk}"
+
+
+class Characteristic(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name}  - #{self.pk}"
+
+
+class FootballClub(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    description = models.CharField(max_length=1000)
+    city = models.CharField(max_length=100)
+    attendance = models.IntegerField(null=True)
+
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    league = models.ForeignKey(League, on_delete=models.CASCADE)
+    characteristic = models.ManyToManyField(Characteristic)
+
+
+    def __str__(self):
+        return f"{self.name}  - #{self.pk}"
